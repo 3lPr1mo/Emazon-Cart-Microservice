ee7583fac14f48b92c232c987b891acf206f7bce
3lPr1mo <erickenriquechaparro@gmail.com>
2024-09-30 17:19:13 -0500

    feat(domain): add domain and exception constants

---
+++ src/main/java/com/bootcamp/pragma/cartmicroservice/domain/util/DomainConstants.java
@@ -0,0 +1,8 @@
+package com.bootcamp.pragma.cartmicroservice.domain.util;
+
+public class DomainConstants {
+    private DomainConstants() {}
+
+    public static final String DATE_TO_RESTOCK_ARTICLE = "Date to restock article ";
+    public static final String ARTICLE_WAS_SAVED_TO_CART_SUCCESSFULLY = "Article was saved successfully";
+}
---
+++ src/main/java/com/bootcamp/pragma/cartmicroservice/domain/util/ExceptionConstants.java
@@ -0,0 +1,7 @@
+package com.bootcamp.pragma.cartmicroservice.domain.util;
+
+public class ExceptionConstants {
+    private ExceptionConstants() {}
+
+    public static final String MAX_CATEGORIES_MSG = "Categories on cart are below the limit";
+}
