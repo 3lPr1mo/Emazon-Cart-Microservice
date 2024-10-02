e96cac31a66a946a893fa5ea707e422099bc112b
3lPr1mo <erickenriquechaparro@gmail.com>
2024-10-02 12:26:50 -0500

    refactor(domain): constant to num max of categories on cart

--- src/main/java/com/bootcamp/pragma/cartmicroservice/domain/util/DomainConstants.java
+++ src/main/java/com/bootcamp/pragma/cartmicroservice/domain/util/DomainConstants.java
@@ -5,4 +5,5 @@ public class DomainConstants {
 
     public static final String DATE_TO_RESTOCK_ARTICLE = "Date to restock article ";
     public static final String ARTICLE_WAS_SAVED_TO_CART_SUCCESSFULLY = "Article was saved successfully";
+    public static final int NUM_MAX_OF_CATEGORIES_ON_CART = 3;
 }
