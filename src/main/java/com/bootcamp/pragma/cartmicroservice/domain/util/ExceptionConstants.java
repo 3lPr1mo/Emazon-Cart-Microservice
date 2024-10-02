0814078d5349eb2b112c47d17c2d805ebabf1220
3lPr1mo <erickenriquechaparro@gmail.com>
2024-10-02 12:27:31 -0500

    refactor(domain): add constant user have already an active cart

--- src/main/java/com/bootcamp/pragma/cartmicroservice/domain/util/ExceptionConstants.java
+++ src/main/java/com/bootcamp/pragma/cartmicroservice/domain/util/ExceptionConstants.java
@@ -4,4 +4,5 @@ public class ExceptionConstants {
     private ExceptionConstants() {}
 
     public static final String MAX_CATEGORIES_MSG = "Categories on cart are below the limit";
+    public static final String USER_HAVE_ALREADY_AN_ACTIVE_CART = "User has already an active cart";
 }
